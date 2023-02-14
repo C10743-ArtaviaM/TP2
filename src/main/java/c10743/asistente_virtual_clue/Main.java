bdcbc66085e23353d202800d8e80e5890ae0e626
C10743-ArtaviaM <112446844+C10743-ArtaviaM@users.noreply.github.com>
2023-02-14 08:41:30 -0600

    Add files via upload

---
+++ src/main/java/c10743/asistente_virtual_clue/Main.java
@@ -0,0 +1,18 @@
+package c10743.asistente_virtual_clue;
+
+/**
+ * Clase Main del programa Asistente Virtual CLUE.
+ * @author Mauricio Artavia Monge | C10743
+ */
+public class Main {
+
+    /**
+     * Metodo main del programa Asistente Virtual CLUE.
+     * @param args Argumentos del programa.
+     */
+    public static void main(String[] args) {
+        // =-=-= Declaracion de variable tipo Partida =-=-= \\
+        Partida game = new Partida();
+        game.game();
+    }
+}
